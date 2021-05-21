b3729b75b96bff7048c97a7b8dc203862da2206b
Nairi <nairi.abgaryan@gmail.com>
2021-05-21 19:49:07 +0400

    FEAT. Config changes for greenhouse

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,14 +1,23 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
 export function getDbConfig(): TypeOrmModuleOptions {
+  const entities = [`${__dirname}/../../modules/**/*.entity{.ts,.js}`]
+  const migrations = [`${__dirname}/../../migrations/*{.ts,.js}`]
+
   return {
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.POSTGRES_HOST,
     port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'greenhouse',
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
     synchronize: true,
     autoLoadEntities: true,
+    entities,
+    migrations,
+    migrationsRun: true,
+    cli: {
+      migrationsDir: 'migration',
+    },
   }
 }
