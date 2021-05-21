a4c73062fe33d8236542d054c54f9f31525993a4
Nairi <nairi.abgaryan@gmail.com>
2021-05-21 19:46:30 +0400

    FEAT. App module changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { DataModule } from './modules/data/data.module'
+import { GreenhouseModule } from './modules/greenhouse/greenhouse.module'
+import { EquipmentModule } from './modules/equipment/equipment.module'
 import { getDbConfig } from './config/db.config'
-import { DataModule } from './data/data.module'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 @Module({
-  imports: [TypeOrmModule.forRoot(getDbConfig()), DataModule],
+  imports: [
+    TypeOrmModule.forRoot(getDbConfig()),
+    DataModule,
+    GreenhouseModule,
+    EquipmentModule,
+  ],
   controllers: [],
   providers: [],
 })
